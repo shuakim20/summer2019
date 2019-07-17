62eb89817183a7b84f662a76eb322d05615afdcf
shuakim20 <52505179+shuakim20@users.noreply.github.com>
2019-07-16 23:10:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert() {
+  alert("I am rushing. Mind your own business.");
+  alert("I am typing. What are you doing?");
+  alert("Don't you see me from the gif? Geesh.");
+  alert("I miss Miss Piggy.");
+  alert("I don't know what I'm doing.");
+  alert("I'm doing my last minute assignments. Wish me LUCK!");
+  }
